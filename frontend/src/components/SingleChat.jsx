4d5c703500d6a9f31b043e33f67d5ff43cb52b98
KaranChawla <chawlakaran663@gmail.com>
2024-03-14 23:21:41 +0530

    change endpoint url

--- frontend/src/components/SingleChat.jsx
+++ frontend/src/components/SingleChat.jsx
@@ -20,7 +20,7 @@ import Lottie from "react-lottie";
 import animationData from "../animations/typing.json";
 
 // const ENDPOINT = "http://localhost:5000";
-const ENDPOINT = "https://talk-a-tive.herokuapp.com";
+const ENDPOINT = "https://mern-chat-app-amfj.onrender.com";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
